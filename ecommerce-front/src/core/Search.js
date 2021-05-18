36baf371530bde584ab77d8d0dd0642394b132d1
ShubhangiMalik <shubhangimalik28@gmail.com>
2021-05-18 14:41:34 +0530

    deployment

--- ecommerce-front/src/core/Search.js
+++ ecommerce-front/src/core/Search.js
@@ -82,7 +82,7 @@ const Search = () => {
    const searchForm = () => (
        <form onSubmit= {searchSubmit}>
            <span className= 'input-group-text'>
-               <div className="input-group input-group">
+               <div className="input-group">
                    <div className="input-group-prepend">
                        <select className= 'btn mr-3' onChange= {handleChange('category')}>
                            <option value="All" className='text-center'> All </option>
